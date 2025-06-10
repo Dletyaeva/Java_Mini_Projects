93ea89ddb2fbb410ab36c6ddafb40b027d0288f8
Daleela <123666979+Dletyaeva@users.noreply.github.com>
2025-06-10 14:44:03 -0500

    made a duplicate of lesson1 projects

---
+++ Lesson1/Copy/1_Plant_a_Tree/Tree/src/Tree.java
@@ -0,0 +1,12 @@
+// TODO: create a tree class
+
+    //TODO: create main method
+
+    /**
+     * TODO:
+     *  1. create a print statement introducing yourself - output to terminal
+     *  2. create a print statement about planting trees - output to terminal
+     *  3. print a tree to screen
+     */
+    
+  
