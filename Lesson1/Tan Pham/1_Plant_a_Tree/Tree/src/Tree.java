be3a8d2696b11d222af9fdc2551b7bcb0f6348d5
Daleela <123666979+Dletyaeva@users.noreply.github.com>
2025-06-30 21:48:41 -0500

    Added Tan's work folder back - Dee

---
+++ Lesson1/Tan Pham/1_Plant_a_Tree/Tree/src/Tree.java
@@ -0,0 +1,22 @@
+// TODO: create a tree class
+
+    //TODO: create main method
+
+    /**
+     * TODO:
+     *  1. create a print statement introducing yourself - output to terminal
+     *  2. create a print statement about planting trees - output to terminal
+     *  3. print a tree to screen
+     */
+   // Define your tree class in this file!
+public class Tree{
+    public static void main(String[] args){
+      //Planting a Tree
+      System.out.println("Hello, I'm Tan");
+      System.out.println("I'm planting a tree");
+      System.out.println("      *****\n       ***\n        *");
+      System.out.println("##################");
+  
+    }
+  }
+  
